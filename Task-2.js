65eedb9eec02d242dd33e395245f521e0c66f69c
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-12 12:02:33 +0530

    Create Task-2.js

---
+++ Task-2.js
@@ -0,0 +1,7 @@
+// Task :
+// Using Js, write a code for twitter in which cut-down the words after 140th char.
+
+var message = prompt("Enter your message!");
+// var length = message.length;
+var messagetill = message.slice(0,140);
+alert(messagetill);
