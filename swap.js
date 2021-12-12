d5ff0c0491ebc374785b5c21aa36f0739172a15b
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-12 11:51:47 +0530

    Create swap.js

---
+++ swap.js
@@ -0,0 +1,16 @@
+// We have different way to swap any two number.
+
+function test() 
+{
+    var a = "3";
+    var b = "8";
+  
+//   condition for swaping of any two number: 
+    var c = a;
+        a = b;
+        b = c;
+  
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
