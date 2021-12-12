b94f52d9882d8a4d826982726e861f576c8a4da4
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-12 11:58:34 +0530

    Create Task.js

---
+++ Task.js
@@ -0,0 +1,5 @@
+// Creating the Twitter character count functionality:
+
+var text = prompt("Enter the text");
+var textcount = text.length;
+alert("the charater you used till now is : " + textcount + ",the remaing charater is " + (140 - textcount));
