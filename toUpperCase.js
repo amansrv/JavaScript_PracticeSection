3a66e9f36606a2336e11221f30076f6f457432bc
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-13 22:54:32 +0530

    Update toUpperCase.js

--- toUpperCase.js
+++ toUpperCase.js
@@ -1,4 +1,4 @@
-// This is another kind of keyword which help us to capitalize the charter in the string
+// This is another kind of keyword which help us to capitalize the charters in the string
 
 var name = "aman.srivastava";
 name.toUpperCase();
