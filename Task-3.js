e9f1d00e28c08d4701842c8ab8e2fee52c2f191e
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-14 02:03:40 +0530

    Create Task-3.js

---
+++ Task-3.js
@@ -0,0 +1,10 @@
+// Task:
+// Use the given formula and find out the age of human & formula is: **H.A = (D.A - 2) *4 +21**
+
+Step 1: use prompt() keyword
+Step 2: enter age of dog and give alert
+Step 3: results: human age
+
+var DogAge = prompt("Enter the age of dog");
+var HumanAge = ((DogAge - 2) *4) +21
+alert("If the age of dog is : " + DogAge + " ,then the age of human will be : " + HumanAge);
