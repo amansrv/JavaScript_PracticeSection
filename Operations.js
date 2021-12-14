d0bd1d646fa02d57661931004f8b785812253881
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-14 23:00:40 +0530

    Create Operations.js

---
+++ Operations.js
@@ -0,0 +1,16 @@
+// There are different operations in JavaScript
+
+// addition
+•	var addtion = 5+5;
+
+// subtraction
+•	var subtraction = 5-5;
+
+// multiplications
+•	var multiplications = 5*5;
+
+// divide
+•	var divide = 5/5;
+
+// modules: tells the reminder
+•	var modules = 5%5;
