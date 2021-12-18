15b3cb7f62f4af53e411418ae98fa579e3373ceb
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-19 02:39:45 +0530

    Update Random-number-generation.js

--- Random-number-generation.js
+++ Random-number-generation.js
@@ -1,5 +1,10 @@
 // one more **Math.random()** keyword!
-// here, number will print form 0 to 0.999999999.... [will print all random number excluding 1]
 
+// here, number will print form 0 to 0.999999999.... [will print all random number excluding 1]
 var n = Math.random();
 console.log(n);
+
+// the number will print in the form of integer.
+var num = Math.random() * 100;
+num = Math.floor(num)   // ---> Due to this keyword number will print in int form.
+console.log(num);
